9e79dba03d944e3c6d2ffe505ef935891677c2ce
YuezhuXu <xu1732@purdue.edu>
2025-11-06 18:03:44 -0500

    Add some demo
    For robust training and robustness quantification

--- examples/tutorials/part_4_Lipschitz_estimator.py
+++ examples/tutorials/part_4_Lipschitz_estimator.py
@@ -56,17 +56,17 @@ est_n.generate_random_weights([10,40,3])
 # 1
 lip_trivial_t = est_t.estimate(method='trivial')
 lip_fast_t = est_t.estimate(method='ECLipsE_Fast')
-# lip_acc = est_t.estimate(method='ECLipsE') # dies
+lip_acc = est_t.estimate(method='ECLipsE') # dies
 
 # show results
 print('For estimator created by a torch model.')
 print(f'Trivial Lipschitz estimate = {lip_trivial_t}')
 print(f'Lipschitz estimate from EClipsE Fast = {lip_fast_t}')
-#print(f'Lipschitz estimate from EClipsE = {lip_acc_t}')
+print(f'Lipschitz estimate from EClipsE = {lip_acc_t}')
 
 # relative tightness
 print(f'Ratio Fast = {lip_fast_t / lip_trivial_t}')
-#print(f'Ratio Acc = {lip_acc_t / lip_trivial_t}')
+print(f'Ratio Acc = {lip_acc_t / lip_trivial_t}')
 
 
 # 2
@@ -77,27 +77,27 @@ lip_fast_w = est_w.estimate(method='ECLipsE_Fast')
 print("For estimator created by given weights.")
 print(f'Trivial Lipschitz estimate = {lip_trivial_w}')
 print(f'Lipschitz estimate from EClipsE Fast = {lip_fast_w}')
-#print(f'Lipschitz estimate from EClipsE = {lip_acc_w}')
+print(f'Lipschitz estimate from EClipsE = {lip_acc_w}')
 
 
 # relative tightness
 print(f'Ratio Fast = {lip_fast_w / lip_trivial_w}')
-#print(f'Ratio Acc = {lip_acc_w / lip_trivial_w}')
+print(f'Ratio Acc = {lip_acc_w / lip_trivial_w}')
 
 
 # 3
 lip_trivial_n = est_n.estimate(method='trivial')
 lip_fast_n = est_n.estimate(method='ECLipsE_Fast')
-# lip_acc_n = est_n.estimate(method='ECLipsE') 
+lip_acc_n = est_n.estimate(method='ECLipsE') 
 
 # show results
 print('For estimator created by nothing.')
 print(f'Trivial Lipschitz estimate = {lip_trivial_n}')
 print(f'Lipschitz estimate from EClipsE Fast = {lip_fast_n}')
-#print(f'Lipschitz estimate from EClipsE = {lip_acc_n}')
+print(f'Lipschitz estimate from EClipsE = {lip_acc_n}')
 
 # relative tightness
 print(f'Ratio Fast = {lip_fast_n / lip_trivial_n}')
-#print(f'Ratio Acc = {lip_acc_n / lip_trivial_n}')
+print(f'Ratio Acc = {lip_acc_n / lip_trivial_n}')
 
 
